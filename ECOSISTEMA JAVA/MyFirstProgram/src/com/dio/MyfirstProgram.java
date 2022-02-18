590af2e85b805fe58e783e36b2464b841e4840b1
JoasJones <joasjones.sousa@gmail.com>
2022-02-17 21:37:12 -0300

    AULA DE PRIMEIROS COMANDOS EM JAVA

---
+++ ECOSISTEMA JAVA/MyFirstProgram/src/com/dio/MyfirstProgram.java
@@ -0,0 +1,18 @@
+package com.dio;
+
+import com.dio.base.Order;
+public class MyfirstProgram {
+	
+	
+
+
+
+	public static void main(String[] args) {
+		
+	
+		Order order = new Order ("Code1234");
+		System.out.println(order);
+	}
+	
+
+}
