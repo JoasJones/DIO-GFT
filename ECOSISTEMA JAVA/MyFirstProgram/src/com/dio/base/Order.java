336db87b8814998199bdeca292fea13394c13980
JoasJones <joasjones.sousa@gmail.com>
2022-02-19 22:48:15 -0300

    java doc

--- ECOSISTEMA JAVA/MyFirstProgram/src/com/dio/base/Order.java
+++ ECOSISTEMA JAVA/MyFirstProgram/src/com/dio/base/Order.java
@@ -1,17 +1,37 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+import java.util.Formatter.BigDecimalLayoutForm;
 
+/**
+ * 
+ * @author JOÁS JONES 
+ * @version 1.0.0
+ * @see BigDecimal
+ * @since Release 10.0.0
+ *
+ */
 
 public class Order {
 	
+	// definição das variaveis 
 	private final String code;
 	private final double totalValue;
+	private final BigDecimal totalValue2;
+	
 	
 	private String[] itens;
 	
-	public Order(String code, double totalValue) {
+	/**
+	 * CONSTRUTOR DA CLASSE 
+	 * @param code codigo do pedido
+	 * @param totalValue valor total do pedido double
+	 * @param totalValue2 valor total do pedido double BigDecimal
+	 */
+	public Order(String code, double totalValue, BigDecimal totalValue2) {
 		this.code = code;
 		this.totalValue = totalValue;
+		this.totalValue2 = totalValue2;
 		
 	}
 	
@@ -22,6 +42,22 @@ public class Order {
 		}
 			
 		
+	}
+	
+	/**
+	 * CALCULA AS TAXAS DE ACORDO COM O VALOR TOTAL DO PEDIDO. SE O PEDIDO FOR MAIOR QUE 100 UMA TAVA É COBRADA
+	 * 
+	 * @return VALOR TOTAL DO PEDIDO COM AS TAXAS
+	 * @throws RuntimeException CASO O VALOR DO PEDIDO FOR NEGATIVO 
+	 */
+	public BigDecimal calculateFee() throws RuntimeException{
+		 if(this.totalValue2.signum() <0) {
+			 throw new RuntimeException("O Pedido não pode ter valor negativo");
+		 }
+		 if(this.totalValue2.compareTo(new BigDecimal("100.00"))>100) {
+			 return this.totalValue2.multiply(new BigDecimal("0.99"));
+		}
+		 return this.totalValue2;
 	}
 	
 	/*
